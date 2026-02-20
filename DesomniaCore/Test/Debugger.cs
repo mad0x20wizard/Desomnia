6245c0f7401fd84c645fc65d3d714dfe9f1195b9
Kevin Koslowski <kevin@madwizard.de>
2026-02-20 17:04:24 +0100

    moved Test.Debugger into core

---
+++ DesomniaCore/Test/Debugger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadWizard.Desomnia.Test
+{
+    public static class Debugger
+    {
+        public static async Task UntilAttached()
+        {
+            Console.WriteLine("Waiting for debugger to attach");
+
+            while (!System.Diagnostics.Debugger.IsAttached)
+                await Task.Delay(100);
+
+            Console.WriteLine("Debugger attached");
+        }
+    }
+}
